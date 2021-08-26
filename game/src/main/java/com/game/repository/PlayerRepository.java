8a83fbe428b59e7cdc903a8ba6514d7f9ff47382
Vitalik Zhukau <unicraft1989@gmail.com>
2021-08-26 20:13:03 +0300

    Add Player in model and learn database

---
+++ game/src/main/java/com/game/repository/PlayerRepository.java
@@ -0,0 +1,8 @@
+package com.game.repository;
+
+import com.game.model.Player;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PlayerRepository { //extends CrudRepository<Player, Long> {
+
+}
